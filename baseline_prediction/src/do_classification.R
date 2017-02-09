d2174cf7db8cb49e103a5b7f70e906bc0299adcf
Gustavo Sudre <gustavo.sudre@nih.gov>
2017-02-09 16:32:14 -0500

    Some initial results with all DTI

---
+++ baseline_prediction/src/do_classification.R
@@ -0,0 +1,135 @@
+# setting up parallelization
+registerDoParallel(ncores,cores=ncores)
+getDoParWorkers()
+
+# spitting out informative stuff
+gf_fname
+data_fname
+group_var
+metric
+default_preproc
+
+rndForestAll = c()
+lrAll = c()
+lsvmAll = c()
+rsvmAll = c()
+xgbAll = c()
+gbmAll = c()
+save_list = c()
+for (m in run_models) {
+  save_list = c(save_list, sprintf('%sFit', m))
+}
+
+# for each train/test split
+for (i in 1:length(inTrain)) {
+  cat(sprintf('\nWorking on split %d of %d', i, length(inTrain)))
+  Xtrain = ldata[ inTrain[[i]], ]
+  ytrain = groups[ inTrain[[i]]]
+  Xtest  = ldata[-inTrain[[i]], ]
+  ytest = groups[-inTrain[[i]]]
+  
+  if ('rndForest' %in% run_models) {
+    ptm <- proc.time()
+    cat('\nRunning random forests\n')
+    rndForestGrid <- expand.grid(.mtry=c(1:sqrt(ncol(Xtrain))))
+    rndForestFit <- train(Xtrain, ytrain,
+                          method = "rf",
+                          trControl = ctrl_cv,
+                          tuneGrid=rndForestGrid,
+                          metric = metric,
+                          preProc = default_preproc)
+    rndForestRes = eval_model(rndForestFit, Xtest, ytest)
+    rndForestAll = rbind(rndForestAll, rndForestRes)
+    print(proc.time() - ptm)
+  }
+  
+  if ('lr' %in% run_models) {
+    ptm <- proc.time()
+    cat('\nRunning logistic regression\n')
+    lrGrid <- expand.grid(.nIter=seq(1,50,5))
+    lrFit <- train(Xtrain, ytrain,
+                   method = "LogitBoost",
+                   trControl = ctrl_cv,
+                   tuneGrid=lrGrid,
+                   metric = metric,
+                   preProc = default_preproc)
+    lrRes = eval_model(lrFit, Xtest, ytest)
+    lrAll = rbind(lrAll, lrRes)
+    print(proc.time() - ptm)
+  }
+  
+  if ('lsvm' %in% run_models) {
+    ptm <- proc.time()
+    cat('\nRunning Linear SVM\n')
+    lsvmGrid <- expand.grid(.C=c(.01, .1, 1, 10, 100, 10^3, 10^4))
+    lsvmFit <- train(Xtrain, ytrain,
+                     method = "svmLinear",
+                     trControl = ctrl_cv,
+                     tuneGrid=lsvmGrid,
+                     metric = metric,
+                     preProc = default_preproc)
+    lsvmRes = eval_model(lsvmFit, Xtest, ytest)
+    lsvmAll = rbind(lsvmAll, lsvmRes)
+    print(proc.time() - ptm)
+  }
+  
+  if ('rsvm' %in% run_models) {
+    ptm <- proc.time()
+    cat('\nRunning RBF SVM\n')
+    rsvmGrid <- expand.grid(.C=seq(1e-2, 1e+4, length.out=7),
+                            .sigma=seq(1e-5, 1e+1, length.out=7))
+    rsvmFit <- train(Xtrain, ytrain,
+                     method = "svmRadial",
+                     trControl = ctrl_cv,
+                     tuneGrid=rsvmGrid,
+                     metric = metric,
+                     preProc = default_preproc)
+    rsvmRes = eval_model(rsvmFit, Xtest, ytest)
+    rsvmAll = rbind(rsvmAll, rsvmRes)
+    print(proc.time() - ptm)
+  }
+  
+  if ('xgb' %in% run_models) {
+    ptm <- proc.time()
+    cat('\nRunning XGBoost\n')
+    xgbGrid <- expand.grid(.nrounds = 1000,
+                           .eta = c(.0001, .001, 0.01, 0.05, 0.1),
+                           .max_depth = c(2,4,6,8,10,14),
+                           .gamma = 1,
+                           .colsample_bytree=.8,
+                           .min_child_weight=1)
+    xgbFit <- train(Xtrain, ytrain,
+                    method = "xgbTree",
+                    trControl = ctrl_cv,
+                    tuneGrid=xgbGrid,
+                    metric = metric,
+                    preProc = default_preproc)
+    xgbRes = eval_model(xgbFit, Xtest, ytest)
+    xgbAll = rbind(xgbAll, xgbRes)
+    print(proc.time() - ptm)
+  }
+  
+  if ('gbm' %in% run_models) {
+    ptm <- proc.time()
+    cat('\nRunning GBM\n')
+    gbmGrid <-  expand.grid(.interaction.depth = c(1:sqrt(ncol(Xtrain))),
+                            .n.trees = seq(1,501,10),
+                            .shrinkage = seq(.0005, .05,.0005),
+                            .n.minobsinnode = 10) #c(5, 10, 15, 20))
+    gbmFit <- train(Xtrain, ytrain,
+                    method = "gbm",
+                    verbose=F,
+                    trControl = ctrl_cv,
+                    tuneGrid=gbmGrid,
+                    metric = metric,
+                    preProc = default_preproc)
+    gbmRes = eval_model(gbmFit, Xtest, ytest)
+    gbmAll = rbind(gbmAll, gbmRes)
+    print(proc.time() - ptm)
+  }
+  
+  # saving fit models
+  fname = sprintf('%s_split%02d.RData', root_fname, i)
+  save(list=save_list, file=fname)
+}
+stopImplicitCluster()
---
+++ baseline_prediction/src/do_metrics_plots.R
@@ -0,0 +1,17 @@
+compile_metrics = function(res, col, hdr) {
+  b = vector()
+  for (d in res) {
+    b = cbind(b, d[,col])
+  }
+  colnames(b) = hdr
+  return(b)
+}
+mylist = list(rndForestAll, lrAll, lsvmAll, rsvmAll, xgbAll, gbmAll)
+hdr = run_models
+par(mfrow=c(2, 3))
+boxplot(compile_metrics(mylist, 1, hdr), ylab='logLoss', las=2)
+boxplot(compile_metrics(mylist, 2, hdr), ylab='AUC', las=2)
+boxplot(compile_metrics(mylist, 3, hdr), ylab='Accuracy', las=2)
+boxplot(compile_metrics(mylist, 4, hdr), ylab='Kappa', las=2)
+boxplot(compile_metrics(mylist, 5, hdr), ylab='Sensitivity', las=2)
+boxplot(compile_metrics(mylist, 6, hdr), ylab='Specificity', las=2)
