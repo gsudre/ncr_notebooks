08ff1e41d7f99d6136c78f0556ad9ab920414881
Gustavo Sudre <gustavo.sudre@nih.gov>
2017-03-10 09:55:11 -0500

    Replaced do_metrics_plot by function

--- baseline_prediction/src/aux_functions.R
+++ baseline_prediction/src/aux_functions.R
@@ -1,5 +1,34 @@
 library(caret)
 
+compile_metrics = function(res, col, hdr) {
+  b = vector()
+  for (d in res) {
+    b = cbind(b, d[,col])
+  }
+  colnames(b) = hdr
+  return(b)
+}
+
+do_metrics_plots = function(root_fname) {
+  load(sprintf('%s.RData', root_fname))
+  res = collect_results(root_fname)
+  mylist = res[[1]]
+  run_models = res[[2]]
+  
+  par(mfrow=c(2, 3))
+  boxplot(compile_metrics(mylist, 1, run_models), ylab='logLoss', las=2)
+  boxplot(compile_metrics(mylist, 2, run_models), ylab='AUC', las=2)
+  boxplot(compile_metrics(mylist, 3, run_models), ylab='Accuracy', las=2, ylim=c(.4, 1))
+  library(Hmisc)
+  res = binconf(x=max(table(groups)), n=length(groups), alpha=.05)
+  abline(h=res[1], col='red')
+  abline(h=res[2], col='red', lty=2)
+  abline(h=res[3], col='red', lty=2)
+  boxplot(compile_metrics(mylist, 4, run_models), ylab='Kappa', las=2)
+  boxplot(compile_metrics(mylist, 5, run_models), ylab='Sensitivity', las=2)
+  boxplot(compile_metrics(mylist, 6, run_models), ylab='Specificity', las=2)
+}
+
 merge_func = function(id, df1, df2, my_ids, x.id, x.date, y.id, y.date) {
   eval(parse(text=sprintf('d1 = subset(df1, %s==id)', x.id)))
   eval(parse(text=sprintf('d2 = subset(df2, %s==id)', y.id)))
