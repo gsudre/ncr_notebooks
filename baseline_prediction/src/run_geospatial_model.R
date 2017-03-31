2f8949e9d2fdc79e15cd3dde78d040b23bb98e30
Gustavo Sudre <gustavo.sudre@nih.gov>
2017-03-31 15:22:05 -0400

    separated other binary function as well

--- baseline_prediction/src/run_geospatial_model.R
+++ baseline_prediction/src/run_geospatial_model.R
@@ -41,61 +41,6 @@ y = merged$DX_BASELINE
 y[y != 'NV'] = 'ADHD'
 y = factor(y, levels = c('NV', 'ADHD'))
 
-# save X and y if not already done so
-fname = sprintf('%s_Xy.RData', root_fname)
-if(!file.exists(fname)){
-  save(X, y, file=fname, compress=T)
-}
-
-set.seed(myseed)
-split <- createDataPartition(y, p = .8, list = FALSE)
-Xtrain <- X[ split, ]
-ytrain <- y[ split ]
-Xtest  <- X[-split, ]
-ytest = y[-split]
-
-pp = preProcess(Xtrain, method=c('YeoJohnson', 'center', 'scale'))
-filtXtrain = predict(pp, Xtrain)
-nearZeroVar(filtXtrain)
-correlations = cor(filtXtrain)
-
-highCorr = findCorrelation(correlations, cutoff=.75)
-length(highCorr)
-noncorrXtrain = filtXtrain[, -highCorr]
-noncorrXtest = predict(pp, Xtest)[, -highCorr]
-
-library(pROC)
-
-set.seed(myseed)
-index <- createMultiFolds(ytrain, k = 5, times = 5)
-
-library(doMC)
-ncpus <- detectBatchCPUs()
-njobs <- ncpus - cpuDiff
-registerDoMC(njobs)
-selFunc = 'best'  # maybe try oneSE and tolerance as well?
-
-set.seed(myseed)
-fullCtrl <- trainControl(method = "repeatedcv",
-                         index = index,
-                         search='grid',
-                         summaryFunction = twoClassSummary,
-                         classProbs = TRUE)
-
-ptm <- proc.time()
-m1 <- train(noncorrXtrain, ytrain,
-            method = mymod,
-            trControl = fullCtrl,
-            tuneLength = tuneLength,
-            metric = 'ROC')
-print(proc.time() - ptm)
-m1
-pred = predict(m1, noncorrXtest)
-postResample(pred, ytest)
-roc(as.numeric(ytest), as.numeric(pred))
-
-fname = sprintf('%s_%04d.RData', root_fname, myseed)
-save_list = c('m1', 'myseed', 'index', 'split')
-save(list=save_list, file=fname)
+source('~/ncr_notebooks/baseline_prediction/src/do_binary_classification.R')
 
 sink()
