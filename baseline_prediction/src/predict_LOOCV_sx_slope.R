82a5430c5594107295e93ee42dcaca873dd7034c
Gustavo Sudre <gustavo.sudregp@nih.gov>
2017-06-02 13:36:51 -0400

    symptom slopes

---
+++ baseline_prediction/src/predict_LOOCV_sx_slope.R
@@ -0,0 +1,84 @@
+args = commandArgs(trailingOnly=TRUE)
+s=as.numeric(args[1])
+ntimes = 2
+model = args[2]
+mysx = args[3]
+myseed = 1234
+tuneLength = 10
+cpuDiff = 0
+
+# out_fname = sprintf('/data/NCR_SBRB/loocv/%s_sx_slope_%s/s%03d.log', mysx, model, s)
+out_fname = sprintf('~/tmp/s%03d.log', s)
+sink(out_fname, append=FALSE, split=TRUE)
+
+get_SX_slope = function (df, ids) {
+  inatt = c()
+  hi = c()
+  mrns = c()
+  for (s in ids) {
+    idx = df$MRN==s
+    # proceed if we have more than one observation in the data
+    if (sum(idx) >= 2) {
+      inatt = c(inatt, lm(SX_inatt ~ age, data=df[idx,])$coefficients[[2]])
+      hi = c(hi, lm(SX_HI ~ age, data=df[idx,])$coefficients[[2]])
+      mrns = c(mrns, s)
+    }
+  }
+  res = cbind(inatt, hi)
+  colnames(res) = c('inatt', 'hi')
+  rownames(res) = mrns
+  return(res)
+}
+source('~/ncr_notebooks/baseline_prediction/src/load_voting_data.R')
+
+library(caret)
+library(doMC)
+ncpus <- detectBatchCPUs()
+njobs <- ncpus - cpuDiff
+registerDoMC(njobs)
+slopes = get_SX_slope(gf, merged$MRN)
+
+X = cbind(prs, geospatial, neuropsych, struct_rois, dti_tracts)
+y = slopes[, mysx]
+
+Xtrain <- X[-s, ]
+ytrain <- y[-s]
+Xtest  <- X[s, ]
+ytest  <- y[s]
+
+# remove training examples that are all NaN. Note that this will never happen for testing!
+rm_me = rowSums(is.na(Xtrain)) == ncol(Xtrain)
+Xtrain = Xtrain[!rm_me,]
+ytrain = ytrain[!rm_me]
+
+set.seed(myseed)
+fullCtrl <- trainControl(method = "boot",
+                         number = ntimes,
+                         savePredictions="final",
+                         summaryFunction=defaultSummary)
+set.seed(myseed)
+mymod = train(Xtrain, ytrain,
+               tuneLength=tuneLength,
+               trControl=fullCtrl,
+               method=model,
+               preProcess = c('medianImpute'))
+
+means = colMeans(slopes)
+print(sprintf('NIR %s:', mysx))
+RMSE(means[mysx], slopes[, mysx])
+
+pred = predict(mymod, newdata=Xtest)
+
+mymod
+myseed
+tuneLength
+ntimes
+pred
+sink()
+
+
+
+
+
+
+
