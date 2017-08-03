7c83b3a5ec06976e54f396a241b4e53ec2f2317e
Gustavo Sudre <gustavo.sudregp@nih.gov>
2017-08-03 11:19:15 -0400

    script to condesne Freesurfer structures

---
+++ prs/condense_struct.R
@@ -0,0 +1,154 @@
+regional = read.csv('~/data/prs/REGIONAL_ANALYSES_FREESURFER.csv')
+
+condense_sublobar = function(ldata) {
+  sublobar_regions = unique(regional$subloar)
+  # for each region, add or average the columns corresponding to the ROIs
+  area_data = c()
+  hdr = c()
+  volume_data = c()
+  thickness_data = c()
+  aparc_data = c()
+  aparc_hdr = c()
+  for (s in sublobar_regions) {
+    if (s != "") {
+      idx = which(regional$subloar == s)
+      area = 0
+      volume = 0
+      thickness = 0
+      aparc = 0
+      for (i in idx) {
+        cnt = 0
+        roi_name = sub('_thickness', '', regional[i, 1])
+        if (length(grep('rh_|lh_', roi_name)) > 0) {
+          var_name = quote(sprintf('%s_area', roi_name))
+          area = area + ldata[, eval(var_name)]
+          var_name = quote(sprintf('%s_volume', roi_name))
+          volume = volume + ldata[, eval(var_name)]
+          var_name = quote(sprintf('%s_thickness', roi_name))
+          thickness = thickness + ldata[, eval(var_name)]
+          cnt = cnt + 1
+        }
+        else {
+          var_name = quote(roi_name)
+          aparc = aparc + ldata[, eval(var_name)]
+        }
+      }
+      if (sum(area) > 0) {
+        hdr = c(hdr, s)
+        area_data = cbind(area_data, area)
+        volume_data = cbind(volume_data, volume)
+        thickness_data = cbind(thickness_data, thickness / cnt)
+      }
+      else {
+        aparc_hdr = c(aparc_hdr, s)
+        aparc_data = cbind(aparc_data, aparc)
+      }
+    }
+  }
+  colnames(area_data) = hdr
+  colnames(volume_data) = hdr
+  colnames(thickness_data) = hdr
+  colnames(aparc_data) = aparc_hdr
+}
+
+condese_lobar = function(ldata) {
+  lobar_regions = unique(regional$lobar)
+  # for each region, add or average the columns corresponding to the ROIs
+  areaLobar_data = c()
+  hdr = c()
+  volumeLobar_data = c()
+  thicknessLobar_data = c()
+  aparcLobar_data = c()
+  aparc_hdr = c()
+  for (s in lobar_regions) {
+    if (s != "") {
+      idx = which(regional$lobar == s)
+      area = 0
+      volume = 0
+      thickness = 0
+      aparc = 0
+      for (i in idx) {
+        cnt = 0
+        roi_name = sub('_thickness', '', regional[i, 1])
+        if (length(grep('rh_|lh_', roi_name)) > 0) {
+          var_name = quote(sprintf('%s_area', roi_name))
+          area = area + ldata[, eval(var_name)]
+          var_name = quote(sprintf('%s_volume', roi_name))
+          volume = volume + ldata[, eval(var_name)]
+          var_name = quote(sprintf('%s_thickness', roi_name))
+          thickness = thickness + ldata[, eval(var_name)]
+          cnt = cnt + 1
+        }
+        else {
+          var_name = quote(roi_name)
+          aparc = aparc + ldata[, eval(var_name)]
+        }
+      }
+      if (sum(area) > 0) {
+        hdr = c(hdr, s)
+        areaLobar_data = cbind(areaLobar_data, area)
+        volumeLobar_data = cbind(volumeLobar_data, volume)
+        thicknessLobar_data = cbind(thicknessLobar_data, thickness / cnt)
+      }
+      else {
+        aparc_hdr = c(aparc_hdr, s)
+        aparcLobar_data = cbind(aparcLobar_data, aparc)
+      }
+    }
+  }
+  colnames(areaLobar_data) = hdr
+  colnames(volumeLobar_data) = hdr
+  colnames(thicknessLobar_data) = hdr
+  colnames(aparcLobar_data) = aparc_hdr
+}
+
+condense_theory = function(ldata) {
+  theory_regions = unique(regional$ROI_theory_driven)
+  # for each region, add or average the columns corresponding to the ROIs
+  areaTheory_data = c()
+  hdr = c()
+  volumeTheory_data = c()
+  thicknessTheory_data = c()
+  aparcTheory_data = c()
+  aparc_hdr = c()
+  for (s in theory_regions) {
+    if (s != "") {
+      idx = which(regional$ROI_theory_driven == s)
+      area = 0
+      volume = 0
+      thickness = 0
+      aparc = 0
+      for (i in idx) {
+        cnt = 0
+        roi_name = sub('_thickness', '', regional[i, 1])
+        if (length(grep('rh_|lh_', roi_name)) > 0) {
+          var_name = quote(sprintf('%s_area', roi_name))
+          area = area + ldata[, eval(var_name)]
+          var_name = quote(sprintf('%s_volume', roi_name))
+          volume = volume + ldata[, eval(var_name)]
+          var_name = quote(sprintf('%s_thickness', roi_name))
+          thickness = thickness + ldata[, eval(var_name)]
+          cnt = cnt + 1
+        }
+        else {
+          var_name = quote(roi_name)
+          aparc = aparc + ldata[, eval(var_name)]
+        }
+      }
+      if (sum(area) > 0) {
+        hdr = c(hdr, s)
+        areaTheory_data = cbind(areaTheory_data, area)
+        volumeTheory_data = cbind(volumeTheory_data, volume)
+        thicknessTheory_data = cbind(thicknessTheory_data, thickness / cnt)
+      }
+      else {
+        aparc_hdr = c(aparc_hdr, s)
+        aparcTheory_data = cbind(aparcTheory_data, aparc)
+      }
+    }
+  }
+  colnames(areaTheory_data) = hdr
+  colnames(volumeTheory_data) = hdr
+  colnames(thicknessTheory_data) = hdr
+  colnames(aparcTheory_data) = aparc_hdr
+}
